eea1916fb80cace385b17b4001d30a0da1355772
klelia <clelia.fradella@gmail.com>
2023-05-26 10:10:18 +0200

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,4 +11,8 @@
         </ul>
     </main>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, commodi.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque quam magni quo ex laudantium, id vitae sint molestiae
+        cumque, repudiandae autem. Recusandae vero autem molestias. Nostrum nemo totam modi architecto quidem harum
+        consequuntur et excepturi quis magni reprehenderit, recusandae eaque debitis minus facere voluptatem. Minima
+        deserunt dolorem quisquam? Impedit, accusantium.</p>
 @endsection
